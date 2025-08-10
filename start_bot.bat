86fd15b6c9c02de07f508078d11ea1ee024770f5
GHILLIExSTEW <kaleb.j.stewart23@gmail.com>
2025-08-10 16:04:05 +0000

    Added Electron

--- start_bot.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting Discord Bot...
-cd /d "c:\Users\Administrator\Desktop\Bot+Server\DBSBM\bot"
-python main.py
-pause
--- start_service_manager.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title DBSBM Service Manager
-cd /d "%~dp0"
-python integrated_service_manager.py
-pause
